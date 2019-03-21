c019e9f4e6738129780d1a5ca1e782d0789b7c3e
jcowman2 <joe.cowman@huskers.unl.edu>
2019-03-20 19:27:49 -0600

    docs(core): In-code docs for io.ts

--- packages/indy-core/src/io.ts
+++ packages/indy-core/src/io.ts
@@ -1,9 +1,26 @@
+/*
+ * Functions for writing formatted messages to stdout/stderr.
+ *
+ * Copyright (c) Joseph R Cowman
+ * Licensed under MIT License (see https://github.com/jcowman2/indy)
+ */
+
+/**
+ * Write a formatted message to the standard output stream.
+ * @param msg The message.
+ */
 export const writeOut = (msg: string) =>
     process.stdout.write(`
 ***
 * INDY: ${msg}
 ***
 `);
+
+/**
+ * Write a formatted message to the standard error stream.
+ * @param msg Error message.
+ * @param error Inner exception, if any.
+ */
 export const writeErr = (msg: string, error?: any) => {
     process.stderr.write(`* INDY: ${msg}\n`);
     if (error !== undefined) {
