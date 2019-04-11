32168bd46cac6bdfb2ea27b77ea8e3a7358a5343
jcowman2 <joe.cowman@huskers.unl.edu>
2019-04-11 15:58:30 -0500

    test(core): Refactor TestableRunner

--- packages/indy-core/test/test-utils.ts
+++ packages/indy-core/test/test-utils.ts
@@ -1,46 +1,33 @@
 import { isAbsolute } from "path";
-import { Runner } from "../";
+import { Runner, RunnerEventData } from "../";
 
 export class TestableRunner {
     public runner: Runner;
     public out: string;
 
     constructor(
-        public doPrint: boolean = false,
-        public doRemoveIndeterminateValues = true
+        public doPrint = false,
+        public doRemoveIndeterminateValues = true,
+        public redactInfo = true,
+        public redactError = false
     ) {
         this.out = "";
         this.runner = new Runner()
-            .on("info", data => {
-                const msg = this.removeIndeterminateValues(data.message);
-                this.out += msg;
-                if (doPrint) {
-                    process.stdout.write(msg);
-                }
-            })
-            .on("debug", data => {
-                const msg = this.removeIndeterminateValues(data.message);
-                this.out += msg;
-                if (doPrint) {
-                    process.stdout.write(msg);
-                }
-            })
-            .on("error", data => {
-                const msg = this.removeIndeterminateValues(data.message);
-                this.out += msg;
-                if (doPrint) {
-                    process.stderr.write(msg);
-                }
-            });
+            .on("info", data => this._handleData(data))
+            .on("debug", data => this._handleData(data))
+            .on("error", data => this._handleData(data, true));
     }
 
     public testSnapshot() {
         expect(this.out).toMatchSnapshot();
     }
 
+    public removeCarriageReturns(str: string) {
+        return str.replace(/\r\n|\r/g, "\n");
+    }
+
     public removeIndeterminateValues(str: string) {
         return str
-            .replace(/\r\n|\r/g, "\n") // Remove carriage returns
             .split(" ")
             .map(word => {
                 // Remove paths
@@ -54,5 +41,30 @@ export class TestableRunner {
                 return word;
             })
             .join(" ");
+    }
+
+    private _handleData(data: RunnerEventData, error: boolean = false) {
+        let message = "";
+
+        const skipCode = error ? 410 : 210;
+        const redact =
+            (error && this.redactError) || (!error && this.redactInfo);
+
+        if (data.code === skipCode && redact) {
+            const stream = error ? "ERR" : "OUT";
+            message = `<Internal Process STD${stream}>\n`;
+        } else {
+            message = this.removeCarriageReturns(data.message);
+            if (this.doRemoveIndeterminateValues) {
+                message = this.removeIndeterminateValues(message);
+            }
+        }
+
+        this.out += message;
+
+        if (this.doPrint) {
+            const stream = error ? process.stderr : process.stdout;
+            stream.write(message);
+        }
     }
 }
