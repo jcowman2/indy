98ebcbcdc175a8e0d7bd45c8f82c4d2c60b9038e
jcowman2 <joe.cowman@huskers.unl.edu>
2019-04-13 14:14:40 -0500

    test(core): Unit tests for SingleDependentImpl.init

--- packages/indy-core/test/unit/dependent.test.ts
+++ packages/indy-core/test/unit/dependent.test.ts
@@ -2,12 +2,14 @@ import {
     SingleDependentArgs,
     singleDependentProvider
 } from "../../src/dependent";
+import { IndyError } from "../../src/events";
 
 const mockDependent = (args: Partial<SingleDependentArgs> = {}) => {
     const defaultArgs = {
         pkg: jest.fn(),
         emitter: {
-            emit: jest.fn()
+            emit: jest.fn(),
+            emitAndThrow: jest.fn()
         },
         processManager: {
             spawnSequence: jest.fn()
@@ -24,7 +26,7 @@ const mockDependent = (args: Partial<SingleDependentArgs> = {}) => {
 
 describe("Dependent", () => {
     describe("Constructor", () => {
-        it("Command lists are empty if not included", () => {
+        test("Command lists are empty if not included", () => {
             const { dependent } = mockDependent();
 
             expect(dependent.initCommands).toEqual([]);
@@ -34,7 +36,7 @@ describe("Dependent", () => {
     });
 
     describe("init", () => {
-        it("Dependent uses its configured initCommands if no argument given", async done => {
+        test("Dependent uses its configured initCommands if no argument given", async done => {
             const initCommands = ["command 1", "command 2"];
             const { dependent, processManager } = mockDependent({
                 initCommands
@@ -49,7 +51,7 @@ describe("Dependent", () => {
             done();
         });
 
-        it("Dependent uses initCommands given by argument if there are any", async done => {
+        test("Dependent uses initCommands given by argument if there are any", async done => {
             const initCommands = ["command 1", "command 2"];
             const overrides = ["command 3"];
             const { dependent, processManager } = mockDependent({
@@ -59,10 +61,48 @@ describe("Dependent", () => {
             await dependent.init(overrides);
 
             expect(processManager.spawnSequence).toHaveBeenCalledWith(
-                initCommands
+                overrides
             );
 
             done();
         });
+    });
+
+    test("Emit init start and init success events if initialization is successful", async done => {
+        const { dependent, emitter } = mockDependent();
+
+        await dependent.init();
+
+        const emitterCalls = (emitter.emit as any).mock.calls;
+
+        expect(emitterCalls[0][0].code).toBe(201);
+        expect(emitterCalls[1][0].code).toBe(202);
+
+        done();
+    });
+
+    test("Emit and throw a fail event if initialization fails", async done => {
+        const failingCommand = "command fails";
+
+        const { dependent, emitter } = mockDependent({
+            initCommands: [failingCommand],
+            processManager: {
+                spawnSequence: jest
+                    .fn()
+                    .mockImplementation((commands: string[]) => {
+                        if (commands.includes(failingCommand)) {
+                            throw new Error("err");
+                        }
+                    }),
+                spawnCommand: jest.fn()
+            }
+        });
+
+        await dependent.init();
+
+        expect((emitter.emit as any).mock.calls[0][0].code).toBe(201);
+        expect((emitter.emitAndThrow as any).mock.calls[0][0].code).toBe(401);
+
+        done();
     });
 });
